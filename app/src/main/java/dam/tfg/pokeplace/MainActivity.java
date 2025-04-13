6d81192c1ffd5a900ac1c1c5c387e15d77381736
Daniel Monforte <danim2205@gmail.com>
2025-04-13 16:39:50 +0200

    Logout button
    Added a logout button in the header. When pressed, the user goes back to MainActivity and if they try to login again, they will be able to choose account again

--- app/src/main/java/dam/tfg/pokeplace/MainActivity.java
+++ app/src/main/java/dam/tfg/pokeplace/MainActivity.java
@@ -1,12 +1,28 @@
 package dam.tfg.pokeplace;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -21,6 +37,16 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
 
+    private ImageView imgFoto;
+    private TextView txtNombre;
+    private TextView txtEmail;
+    private Button btnLogout;
+
+    private String providerId="";
+    private FirebaseUser firebaseUser;
+    private GoogleSignInClient gClient;
+    private GoogleSignInOptions gOptions;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,25 +55,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null)
-                        .setAnchorView(R.id.fab).show();
-            }
-        });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
+        View headerView = navigationView.getHeaderView(0);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setOpenableLayout(drawer)
-                .build();
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow).setOpenableLayout(drawer).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        txtNombre = headerView.findViewById(R.id.txtUser);
+        txtEmail = headerView.findViewById(R.id.txtEmailUser);
+        btnLogout = headerView.findViewById(R.id.btnLogout);
+        imgFoto = headerView.findViewById(R.id.imgUser);
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        providerId = firebaseUser.getProviderData().get(1).getProviderId();
+        if (providerId.equals("google.com")) { //Si el proveedor es Google
+            gOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+            gClient = GoogleSignIn.getClient(this, gOptions);
+        }
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                switch (providerId) {
+                    case "google.com":
+                        gClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Log.i(TAG,"Sesion de Google cerrada");
+                            }
+                        });
+                        break;
+                    case "password":
+                        break;
+                    default:
+                        Toast.makeText(getApplicationContext(), "Error en el ID del proveedor al cerrar sesion", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                finish();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
     }
 
     @Override
